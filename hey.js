6eaaaec5391621d906294e7a9820f3385770bf6a
omosheroye <69145163+omosheroye@users.noreply.github.com>
2020-12-13 22:33:23 +0100

    Update hey.js

--- hey.js
+++ hey.js
@@ -1,13 +1,9 @@
 const panels= document.querySelectorAll('.panel');
-function remove(){
-    this.classList.remove('open');
 
-};
 function opentogg(){
     
     this.classList.toggle('open');
 
 };
 
-panels.forEach(panel => panel.addEventListener('click', remove));
 panels.forEach(panel => panel.addEventListener('click', opentogg));
